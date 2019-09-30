545fe3e6027e538c294b7758f70077e405241827
rbonifacio <rbonifacio@MacBook-Air-de-Edna.local>
2019-09-30 20:51:28 -0300

    support to visitors and type checker

--- oberon/src/main/scala/br/unb/cic/oberon/Expression.scala
+++ oberon/src/main/scala/br/unb/cic/oberon/Expression.scala
@@ -1,43 +1,84 @@
 package br.unb.cic.oberon
 
+import br.unb.cic.oberon.Visitor
+
+
+trait Type
+
+case class TInt() extends Type
+case class TBool() extends Type
+case class TError() extends Type
+
 abstract class Expression {
-  def eval(): Value	 
+  def computeType() : Type 
+  def typeCheck() : Boolean
+  def eval(): Value
+  def accept(v : Visitor) : Unit 
 }
 
 trait Value extends Expression {
   type T
   val value: T
-  
+
+  override def typeCheck(): Boolean = true
+
   override def eval(): Value = this
 }
 
 case class IntValue(v: Integer) extends Value {
-   type T = Integer
-   val value = v
+  type T = Integer
+  val value = v
+
+  override def computeType() : Type = TInt()
+
+  def accept(v : Visitor) {
+    v.visit(this) 
+  }
 }
 
 case class BoolValue(v: Boolean) extends Value {
-   type T = Boolean
-   val value = v
+  type T = Boolean
+  val value = v
+
+  override def computeType() : Type = TBool()
+
+  def accept(v : Visitor) {
+    v.visit(this) 
+  }
+
 }
 
 case class AddExp(val lhs: Expression, val rhs: Expression) extends Expression {
+  override def typeCheck() : Boolean = lhs.computeType() == TInt() && rhs.computeType() == TInt()
+
+  override def computeType() : Type = if(lhs.computeType == TInt() && rhs.computeType() == TInt()) TInt() else TError()
+
   override def eval(): Value = {
     val l = lhs.eval().asInstanceOf[IntValue]
     val r = rhs.eval().asInstanceOf[IntValue]
 
     return new IntValue(l.value + r.value)
   }
+
+  def accept(v : Visitor) {
+    v.visit(this)
+  }
 }
 
-// case class SubExp(val lhs: Expression, val rhs: Expression) extends Expression
-// case class MulExp(val lhs: Expression, val rhs: Expression) extends Expression
-// case class DivExp(val lhs: Expression, val rhs: Expression) extends Expression
+case class AndExp(val lhs: Expression, val rhs: Expression) extends Expression {
+  override def typeCheck() : Boolean = lhs.computeType() == TBool() && rhs.computeType() == TBool()
 
-// case class AndExp(val lhs: Expression, val rhs: Expression) extends Expression
-// case class OrExp (val lhs: Expression, val rhs: Expression) extends Expression
-// case class NotExp(val exp: Expression) extends Expression 
+  override def computeType() : Type = if(lhs.computeType == TBool() && rhs.computeType() == TBool()) TBool() else TError()
 
-// case class EqExp (val lhs: Expression, val rhs: Expression) extends Expression
-// case class NeqExp(val lhs: Expression, val rhs: Expression) extends Expression 
+  override def eval(): Value = {
+    val l = lhs.eval().asInstanceOf[BoolValue]
+    val r = rhs.eval().asInstanceOf[BoolValue]
 
+    return new BoolValue(l.value && r.value)
+  }
+
+  def accept(v : Visitor) {
+    v.visit(this)
+  }
+ 
+}
